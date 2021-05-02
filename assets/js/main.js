357abcd4fab4ffe85c739f7c6c6f8e437b29b1ed
Luiz Cláudio <lc.danella.dev@gmail.com>
2021-05-02 14:48:50 -0300

    Adicionado clear Input e funcao createTask

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,48 @@ const submitElement = document.querySelector("#add-task");
 const todoList = document.querySelector("#todo-list");
 
 function addTask() {
+  createTask();
+  clearImput();
+}
+
+function createTask() {
   const listItem = document.createElement("li");
   const times = document.createElement("i");
   const content = textElement.value;
-  times.classList.add("fas");
-  times.classList.add("fa-times");
+  adicionaClasse(times);
   listItem.textContent = content;
   listItem.appendChild(times);
   todoList.appendChild(listItem);
+  const times2 = document.querySelectorAll("i");
+  times2.forEach(close);
+}
+
+function clearImput() {
+  textElement.value = "";
+  textElement.focus();
+}
+
+function adicionaClasse(element) {
+  element.classList.add("fas");
+  element.classList.add("fa-times");
+}
+
+function close(time) {
+  time.addEventListener("click", function () {
+    time.parentElement.remove();
+  });
 }
 
 textElement.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    addTask();
+  if (textElement.value !== "") {
+    if (e.key === "Enter") {
+      addTask();
+    }
   }
 });
 
 submitElement.addEventListener("click", function () {
-  addTask();
+  if (textElement.value !== "") {
+    addTask();
+  }
 });
